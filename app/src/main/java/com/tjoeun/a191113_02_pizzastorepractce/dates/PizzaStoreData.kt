1debb1b4cb9234a04f3e3722563c59467841a2f5
ducho <ducho5358@gmail.com>
2019-11-13 21:11:43 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a191113_02_pizzastorepractce/dates/PizzaStoreData.kt
@@ -0,0 +1,9 @@
+package com.tjoeun.a191113_02_pizzastorepractce.dates
+
+class PizzaStoreData(name:String, phoneNum:String, logoUrl:String) {
+
+    var name = name
+    var phoneNum = phoneNum
+    var logoUrl = logoUrl
+
+}
